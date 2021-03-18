99ae6916d5bb35cc7efbbaff6b70733590464909
Ghazi <ghaziatie@gmail.com>
2021-03-18 23:07:35 +0100

    Create function checkuser

--- createuser.py
+++ createuser.py
@@ -1,5 +1,7 @@
 import mysql.connector
 
+from main import DB
+
 
 class Createuser:
 
@@ -20,20 +22,28 @@ class Createuser:
     def insertuser(self, user_name):
         try:
             add_user = ("""INSERT INTO User
-                                   (id, name)
-                                 VALUES (%(id)s, %(name)s)""")
-            id = self.cursor.lastrowid
+                                   (name)
+                                 VALUES (%(name)s)""")
             user_data = {
-                'id': id,
                 'name': user_name,
             }
             self.cursor.execute(add_user, user_data)
             self.cnx.commit()
-            print(f"Nom d'utilisateur {user_name} a été crée")
+            print(f"Le nom d'utilisateur {user_name} a été crée")
         except mysql.connector.errors.IntegrityError:
             print("Ce nom d'utilisateur existe déjà")
 
+    def checkuser(self, user_name):
+        sql_select_query = f"SELECT name from User WHERE name = '{user_name}'"
+        self.cursor.execute(sql_select_query)
+        res = self.cursor.fetchall()
+        if res:
+            print("Le nom d'utilisateur est valide")
+        else:
+            print("Le nom d'utilisateur est invalide")
+            exit()
 
-user = Createuser()
-username = input("Veuillez entrer un nom d'utilisateur:")
-user.insertuser(username)
+# user = Createuser()
+# username = input("Veuillez entrer un nom d'utilisateur:")
+# user.insertuser(username)
+# user.checkuser("Ghazi")
